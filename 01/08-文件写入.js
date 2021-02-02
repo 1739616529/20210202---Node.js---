5fb3434f31bd9702813f269384956594c119972d
dysaliy <8612962+dysaliy@user.noreply.gitee.com>
2021-02-02 15:51:10 +0800

    写入文件备注

--- 01/08-文件写入.js
+++ 01/08-文件写入.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 fs.writeFile("data/01.txt", "瓜皮", (err) => {
+  // writeFile()  内部三个参数    文件相对路径      需要写入的内容     回调函数
   if (err) return console.log("写入失败");
+  
   console.log("写入成功");
 });
