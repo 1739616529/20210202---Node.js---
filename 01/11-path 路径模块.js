c51c442ce203191cf1b4a608fc8c48c056ae4a6d
dysaliy <8612962+dysaliy@user.noreply.gitee.com>
2021-02-02 17:31:54 +0800

    path 进退路径

--- 01/11-path 路径模块.js
+++ 01/11-path 路径模块.js
@@ -5,3 +5,13 @@ const path2 = "./20210202   Node.js 学习 ";
 const newPath = path.join(path1, path2);
 
 console.log(newPath);
+
+//当前路径往上退三级
+
+const fileP = path.join(__dirname, "../../../");
+//退几级填几个../
+console.log(fileP);
+
+const fileP2 = path.join(__dirname , '/data')
+//进路径就添加/和路径名
+console.log(fileP2);
