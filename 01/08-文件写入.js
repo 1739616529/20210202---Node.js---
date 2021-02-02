bdba240331521a91add09ad5614b3fb687ea4c21
dysaliy <8612962+dysaliy@user.noreply.gitee.com>
2021-02-02 16:18:43 +0800

    文件写入 传对象

--- 01/08-文件写入.js
+++ 01/08-文件写入.js
@@ -1,7 +1,14 @@
 const fs = require("fs");
-fs.writeFile("data/01.txt", "瓜皮", (err) => {
+let heroList = [
+  { id: 1, name: "武则天", gender: "女" },
+  { id: 2, name: "王昭君", gender: "女" },
+];
+fs.writeFile("data/01.txt", JSON.stringify(heroList), (err) => {
   // writeFile()  内部三个参数    文件相对路径      需要写入的内容     回调函数
   if (err) return console.log("写入失败");
-  
+
   console.log("写入成功");
+});
+fs.readFile("data/01.txt", (err, data) => {
+  console.log(JSON.stringify(data));
 });
