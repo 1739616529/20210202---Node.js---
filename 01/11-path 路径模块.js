8950d34df27e19d347186705a5c2ecca04df98e4
dysaliy <8612962+dysaliy@user.noreply.gitee.com>
2021-02-02 17:24:01 +0800

    path 注释

--- 01/11-path 路径模块.js
+++ 01/11-path 路径模块.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const path1 = "D:\\HTML";
 const path2 = "./20210202   Node.js 学习 ";
+// 如果想返回上一级  就添加../
 const newPath = path.join(path1, path2);
 
 console.log(newPath);
