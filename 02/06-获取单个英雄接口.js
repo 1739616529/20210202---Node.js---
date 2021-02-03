b5c9cfafeadf00339e845d0bfddd915a0ee2f2c3
dysaliy <8612962+dysaliy@user.noreply.gitee.com>
2021-02-03 17:03:52 +0800

    查找单个英雄find()循环查找方法

--- 02/06-获取单个英雄接口.js
+++ 02/06-获取单个英雄接口.js
@@ -14,11 +14,20 @@ const server = http
       //用get获取对象的id值
       console.log(id);
       fs.readFile(heroPath, (err, data) => {
-        if (err) return res.end("获取失败");
+        if (err)
+          return res.end(`{
+          "code":500,
+          "msg":"服务器处理失败"
+          }`);
         const heroArr = JSON.parse(data);
-        const heroItem = heroArr[id - 1];
+        const heroItem = heroArr.find((item) => item.id == id);
+        // res.find((item)=>{}) 是一种带查找功能的遍历 遍历顺便查找值 如果找到就返回
         res.setHeader("content-type", "text/html;charset=utf8");
-        res.end(JSON.stringify(heroItem));
+        res.end(`{
+          "code": 200,
+          "msg": "请求成功",
+          "data": ${JSON.stringify(heroItem)}
+          }`);
       });
     }
   })
